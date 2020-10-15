f31fb669cb77ae4f7f5351054755d4d15025745e
Devin Inbody <devin.inbody@gmail.com>
2020-10-14 21:51:53 -0400

    movie details componentadded

--- src/app/movie-details/movie-details.component.ts
+++ src/app/movie-details/movie-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MovieDataService } from '../movie-data.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-details',
@@ -7,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-details.component.scss'],
 })
 export class MovieDetailsComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+  feeds;
+  ngOnInit() {
+    this.activatedRoute.data.subscribe((data) => {
+      this.feeds = data;
+    });
   }
 }
